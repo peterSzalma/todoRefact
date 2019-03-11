8618f4d71f198bf22cc334899ff18fa2826b91a4
peterSzalma <petiszalma@gmail.com>
2019-03-11 09:50:58 +0100

    add security

---
+++ src/main/java/com/codecool/todorefact/auth0/APIController.java
@@ -0,0 +1,36 @@
+package com.codecool.todorefact.auth0;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Component
+public class APIController {
+
+    @RequestMapping(value = "/api/public", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String publicEndpoint() {
+        return new JSONObject()
+                .put("message", "Hello from a public endpoint! You don\'t need to be authenticated to see this.")
+                .toString();
+    }
+
+    @RequestMapping(value = "/api/private", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String privateEndpoint() {
+        return new JSONObject()
+                .put("message", "Hello from a private endpoint! You need to be authenticated to see this.")
+                .toString();
+    }
+
+    @RequestMapping(value = "/api/private-scoped", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String privateScopedEndpoint() {
+        return new JSONObject()
+                .put("message", "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.")
+                .toString();
+    }
+}
---
+++ src/main/java/com/codecool/todorefact/auth0/AppConfig.java
@@ -0,0 +1,54 @@
+package com.codecool.todorefact.auth0;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@PropertySource(value = "auth0.properties")
+@EnableWebSecurity
+@Configuration
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+    @Value(value = "${auth0.apiAudience}")
+    private String apiAudience;
+    @Value(value = "${auth0.issuer}")
+    private String issuer;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/public").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/private").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/private-scoped").hasAuthority("read:messages");
+    }
+}
